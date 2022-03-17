7bb0c12ecace395a571755f942e5e0d073d94d3d
Andrew Klabukov <Andrew.klabukov@yandex.ru>
2022-03-17 17:21:23 +0400

    change js file

--- script.js
+++ script.js
@@ -1,29 +1,75 @@
+// Задание 1
+
+function concatStrings(string, separator = '') {
+  return function (arg) {
+    if (typeof arg !== 'string') {
+      return string;
+    }
+
+    if (typeof separator !== 'string') {
+      separator = '';
+    }
+
+    let concatenatedString = string + separator + arg;
+
+    return concatStrings(concatenatedString, separator);
+  }
+}
+
+console.log(concatStrings('first', null)('second')())
 
 // Задание 2 
 
 class Calculator {
-  constructor(firstValue, secondValue) {
-    this.firstValue = firstValue,
-    this.secondValue = secondValue
+  constructor(x, y) {   
+    const isValid = typeof x !== 'number' || typeof y !== 'number' || !isFinite(x) || !isFinite(y);
+
+    if (isValid) {
+      throw new Error('Ошибка!')      
+    } 
+    this.x = x,
+    this.y = y
+    this.setX = this.setX.bind(this);
+    this.setY = this.setY.bind(this);
+    this.logSum = this.logSum.bind(this);
+    this.logSub = this.logSub.bind(this);
+    this.logMul = this.logMul.bind(this);
+    this.logDiv = this.logDiv.bind(this);  
+  } 
+
+  setX(num){
+    if (typeof num !== 'number') {
+      throw new Error('Ошибка!')
+    }
+    
+    this.x = num;       
   }
 
-  // setX(num){
-  //   num = 
-  // }
-  // setY(num){
+  setY(num){
+    if (typeof num !== 'number') {
+      throw new Error('Ошибка!')
+    }
+    
+    this.y = num;  
+  }
 
-  // }
   logSum() {
-    console.log(this.firstValue + this.secondValue)
+    console.log(this.x + this.y);
   }
+
   logMul() {
-    console.log(this.firstValue * this.secondValue)
+    console.log(this.x * this.y);
   }
+
   logSub() {
-    console.log(this.firstValue - this.secondValue)
+    console.log(this.x - this.y);
   }
+
   logDiv() {
-    console.log(this.firstValue / this.secondValue)
+    if (this.y === 0) {
+      throw new Error('Ошибка!')
+    }
+    console.log(this.x / this.y);
   }
 
 }
@@ -31,7 +77,7 @@ class Calculator {
 const calculator = new Calculator(12, 3);
 calculator.logSum(); // 15
 calculator.logDiv(); // 4
-// calculator.setX(15);
+calculator.setX(15);
 calculator.logDiv(); // 5
 const logCalculatorDiv = calculator.logDiv;
 logCalculatorDiv(); // 5
